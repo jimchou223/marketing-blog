547f6a22db4317a89240c0cda205c414bca8c8f7
Jim <jimchou223@outlook.com>
2021-03-29 15:42:28 +0800

    add about

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,41 @@
-import React from 'react';
-
-const about = () => {
-    return (
-        <div>
-            
+import React from "react";
+import { fromImageToUrl, API_URL } from "../utils/urls";
+import styles from "../styles/About.module.css";
+const about = (props) => {
+  const author_1 = props.authors[0];
+  const author_2 = props.authors[1];
+  return (
+    <div className="widthController">
+      <div className={styles.authorWrapper}>
+        <div className={styles.textWrapper}>
+          <h3>{author_1.name}</h3>
+          <p>{author_1.intro}</p>
         </div>
-    );
-}
+        <img src={author_1.image} />
+      </div>
+
+      <div className={styles.authorWrapper}>
+        <img src={author_2.image} />
+        <div className={styles.textWrapper}>
+          <h3>{author_2.name}</h3>
+          <p>{author_2.intro}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default about;
+
+export async function getStaticProps() {
+  // fetch the product
+
+  const authors_res = await fetch(`${API_URL}/authors`);
+  const authors = await authors_res.json();
+
+  return {
+    props: {
+      authors,
+    },
+  };
+}
